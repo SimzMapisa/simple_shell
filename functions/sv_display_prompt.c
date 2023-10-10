595f614420306b36dbb1c4c27116cd0e6815b26a
SimzMapisa <simbarashe795@gmail.com>
2023-10-10 12:23:19 +0200

    display prompt icon/sign ($) to the user

---
+++ functions/sv_display_prompt.c
@@ -0,0 +1,15 @@
+#include "sv_shell.h"
+
+/**
+ * displayPrompt - displays prompt icon for user input
+ * Return: 0 success
+ */
+
+int displayPrompt(void)
+{
+	_putchar('(');
+	_putchar('$');
+	_putchar(')');
+
+	return (0);
+}
