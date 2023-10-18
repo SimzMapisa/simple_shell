fe62403d78119930155321ce3307242369dd8c36
SimzMapisa <simbarashe795@gmail.com>
2023-10-18 16:56:02 +0200

    refactor handleExit

--- sv_exit.c
+++ sv_exit.c
@@ -10,7 +10,7 @@
 int handleExitWithArgs(char **args)
 {
 	int exitStatus;
-	char err[256] = "./hsh: 1: exit: Illegal number: ";
+	char error[256] = "./hsh: 1: exit: Illegal number: ";
 
 	if (args[1] != NULL)
 	{
@@ -21,9 +21,9 @@ int handleExitWithArgs(char **args)
 			exit(0);
 		else if (exitStatus < 0 || (exitStatus == 0 && _strcmp(args[1], "0") != 0))
 		{
-			_strcat(err, args[1]);
-			_strcat(err, "\n");
-			write(2, err, _strlen(err));
+			_strcat(error, args[1]);
+			_strcat(error, "\n");
+			write(2, error, _strlen(error));
 			exit(2);
 		}
 	}
@@ -39,25 +39,7 @@ int handleExitWithArgs(char **args)
 
 int handleExit(char **args)
 {
-	int foundExit = 0, i;
-	char *err = "/bin/ls: cannot access '/test_hbtn': No such file or directory";
-
-	for (i = 0; args[i] == NULL; i++)
-	{
-		if (_strcmp(args[i], "exit") == 0)
-		{
-			foundExit += 1;
-			break;
-		}
-	}
 	if (_strcmp(args[0], "exit") == 0 && args[1] == NULL)
 		exit(0);
-	else if (strcmp(args[0], "exit") == 0 && args[1] != NULL)
-		handleExitWithArgs(args);
-	else if (foundExit == 1)
-	{
-		write(2, err, _strlen(err));
-		exit(2);
-	}
 	return (0);
 }
