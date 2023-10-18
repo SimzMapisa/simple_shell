19628558c0109841fb49ff3bec0cd60d1400a8c4
SimzMapisa <simbarashe795@gmail.com>
2023-10-18 15:32:47 +0200

    code restructure: main should handle user input

--- shell_01.c
+++ shell_01.c
@@ -2,15 +2,45 @@
 
 /**
  * main - entry point
- * @argv: array of arguments
- * @argc: length of the array
+ * @av: array of arguments
+ * @ac: length of the array
+ * @env: environmental variables
  *
  * Return: 0 success
  */
 
-int main(int argc, char *argv[])
+int main(int ac, char **av, char **env)
 {
-	(void)argc;
-	handleUserInput(argv);
+	char *input = NULL, *token, *input_cpy;
+	int n_chars;
+	size_t size = 0;
+	char **args;
+	(void)av;
+	(void)ac;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, "> ", 2);
+		fflush(stdout);
+		n_chars = getline(&input, &size, stdin);
+		if (n_chars == -1)
+		{
+			free(input);
+			exit(0);
+		}
+		if (checkTabsOrNewline(input) == 1)
+			continue;
+		if (n_chars > 0 && input[n_chars - 1] == '\n')
+			input[n_chars - 1] = '\0';
+		input_cpy = input;
+		args = splitString(input, " \n");
+		if (_strcmp(args[0], "env") == 0)
+			printEnv(env);
+		handleExit(args);
+		token = strtok(input_cpy, " ");
+		execCmd(token, args, env);
+		free(args);
+	}
 	return (0);
 }
