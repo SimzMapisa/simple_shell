5bfd4b11604b527842d5eb24ce31b18bc531b1f6
SimzMapisa <simbarashe795@gmail.com>
2023-10-18 15:34:45 +0200

    create execute commend function that hndles user input

---
+++ sv_exec.c
@@ -0,0 +1,37 @@
+#include "sv_shell.h"
+
+/*
+ * execCmd - executes the program
+ * @token: tokenised user input
+ * @args: array of argumt strings
+ * @env: environment variables
+ *
+ * Return: Nothing
+ */
+
+void execCmd(char *token, char **args, char **env)
+{
+	pid_t pid;
+	int status;
+	char *cmd;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if ((strcmp(token, "/bin/ls") == 0) || (strcmp(token, "/bin/echo") == 0))
+		{
+			execve(token, args, env);
+			token = strtok(NULL, " ");
+		}
+		cmd = getCommand(args[0]);
+		if (cmd)
+			execve(cmd, args, env);
+		else
+		{
+			perror(cmd);
+			exit(status);
+		}
+	}
+	else
+		wait(&status);
+}
